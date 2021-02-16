08ad254a114089d84bb0b6fa1a0750c45cea9d40
trnlht <trnlht@gmail.com>
2021-02-16 11:27:40 +0300

    has_repetitive_chars_sort func added

--- strings/has_repetitive_chars.py
+++ strings/has_repetitive_chars.py
@@ -12,6 +12,16 @@ def has_repetitive_chars(s):
 
     return False
 
+def has_repetitive_chars_sort(s):
+    sorted_str = sorted(s)
+
+    for i in range(len(sorted_str) - 1):
+        if sorted_str[i] == sorted_str[i+1]:
+            return True
+
+    return False
+
+
     
 class TestHasRepetitiveChars(unittest.TestCase):
 
@@ -22,6 +32,14 @@ class TestHasRepetitiveChars(unittest.TestCase):
 
         self.assertFalse(has_repetitive_chars("abcdefg"))
         self.assertFalse(has_repetitive_chars(""))
+
+    def test_has_repetitive_chars_sort(self):
+        self.assertTrue(has_repetitive_chars_sort("abcdeefg"))
+        self.assertTrue(has_repetitive_chars_sort("abcdefgg"))
+        self.assertTrue(has_repetitive_chars_sort("aabcdefg"))
+
+        self.assertFalse(has_repetitive_chars_sort("abcdefg"))
+        self.assertFalse(has_repetitive_chars_sort(""))
 
 if __name__ == "__main__":
     unittest.main(verbosity=2)
