2d7d890c737915ead0d03c7158851f430ecdf0e5
trnlht <trnlht@gmail.com>
2021-03-29 17:08:01 +0300

    Tests for compress_string addedw

--- strings/compress_string.py
+++ strings/compress_string.py
@@ -2,6 +2,16 @@ import unittest
 
 
 def compress_string(s):
+    """
+    Compress repeating chars in string.
+    Example: "aaaa" -> "a4"
+    Return source string if compressed string is bigger than source string
+
+    :param s: string to compress
+
+    :return: compressed string or source string
+
+    """
     i = 0
     res = ""
     while i < len(s):
@@ -11,9 +21,25 @@ def compress_string(s):
             cnt += 1
             i += 1
 
-        res += s[i] + str(cnt)
+        if cnt > 1:
+            res += s[i] + str(cnt)
+        else:
+            res += s[i]
 
         i += 1
 
     return res if len(res) < len(s) else s
 
+
+class CompressStringTester(unittest.TestCase):
+    def test_compress_string(self):
+        self.assertEqual(compress_string(""), "")
+        self.assertEqual(compress_string("aaa"), "a3")
+        self.assertEqual(compress_string("aa"), "aa")
+        self.assertEqual(compress_string("aaabbc"), "a3b2c")
+        self.assertEqual(compress_string("aaaabbbbccccc"), "a4b4c5")
+
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
