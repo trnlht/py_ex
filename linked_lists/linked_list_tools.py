117942422ec7d1ae61c707e5f84c51d97d80d719
trnlht <trnlht@gmail.com>
2022-10-07 12:21:12 +0300

    Gitignore added. Last changes added.

--- linked_lists/linked_list_tools.py
+++ linked_lists/linked_list_tools.py
@@ -1,8 +1,81 @@
 from dataclasses import dataclass
 
-@dataclass
+import unittest
+
+# @dataclass
+# class Node:
+    # data: int
+    # next: Node
+
 class Node:
-    data: int
-    next: Node
+    def __init__(self, data, next):
+        self.data = data
+        self.next = next
+
+def create_linked_list_from_list(input_list):
+    if not input_list:
+        return None
+
+    head = Node(data=input_list[0], next=None)
+    cur = head
+
+    for val in input_list[1:]:
+        new_node = Node(val, None)
+        cur.next = new_node
+        cur = new_node
+
+    return head
+
+def traverse(head):
+    cur = head
+    while cur != None:
+        yield cur
+        cur = cur.next
+
+def check_linked_list_and_list_equal(linked_list, list):
+    pass
 
 
+
+class CreateLinkedListFromListTester(unittest.TestCase):
+    def test_create_empty_linked_list_from_list(self):
+        self.assertEqual(None, create_linked_list_from_list([]))
+    
+    def test_create_linked_list_from_list(self):
+        input_lists = [[1], [1,2,3], [1,2,3,4,5], [0, 0, 0, 0]]
+
+        for input_list in input_lists:
+            head = create_linked_list_from_list(input_list)
+            i = 0
+            cur = head
+            while cur != None:
+                self.assertEqual(cur.data, input_list[i])
+                cur = cur.next
+                i += 1
+
+class TraverseTester(unittest.TestCase):
+    def test_traverse_empty_list(self):
+        head = create_linked_list_from_list([])
+
+        gen = traverse(head)
+
+        self.assertRaises(StopIteration, next, gen)
+
+    def test_traverse_list(self):
+        input_list = [1, 2, 3]
+        head = create_linked_list_from_list(input_list)
+
+        for node, val in zip(traverse(head), input_list):
+            self.assertEqual(node.data, val)
+
+        i = 0
+        for node in traverse(head):
+            self.assertEqual(node.data, input_list[i])
+            i += 1
+
+
+
+        
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
--- linked_lists/remove_duplicates.py
+++ linked_lists/remove_duplicates.py
@@ -1,22 +1,63 @@
-from linked_list_tools import Node
+import unittest
+from linked_list_tools import Node, create_linked_list_from_list, traverse
 from collections import defaultdict
 
 
 def remove_duplicates(head):
+    """
+    Removes duplicates from linked list.
+    Complexity: O(N)
+    [CCI 2.1]
+
+    :param head: Original list
+    """
     if head == None:
         return head
 
-    prev = head
-    cur = head.next
-    elem_counters = defaultdict(int)
-    elem_counters[prev.data] += 1
+    s = set()
+    s.add(head.data)
+    
+    cur = head
 
-    while cur.next:
-        elem_counters[cur.data] += 1
-        if elem_counters[cur.data] > 1:
-            prev.next = cur.next
+    while cur.next != None:
+        if cur.next.data in s:
+            cur.next = cur.next.next
         else:
-            prev = prev.next
+            s.add(cur.next.data)
+            cur = cur.next
 
-        cur = cur.next
+    return head
+
+class RemoveDuplicatesTester(unittest.TestCase):
+
+    def test_remove_duplicates_from_empty_list(self):
+        head = None
+        self.assertEqual(remove_duplicates(head), None)
+
+    def test_remove_duplicates_from_list(self):
+        head = create_linked_list_from_list([0])
+
+        new_head = remove_duplicates(head)
+
+        expected_list = [0]
+        for node, val in zip(traverse(new_head), expected_list, strict=True):
+            self.assertEqual(node.data, val)
+
+
+if __name__ == "__main__":
+    unittest.main(verbosity=2)
+
+    # prev = head
+    # cur = head.next
+    # elem_counters = defaultdict(int)
+    # elem_counters[prev.data] += 1
+
+    # while cur.next:
+        # elem_counters[cur.data] += 1
+        # if elem_counters[cur.data] > 1:
+            # prev.next = cur.next
+        # else:
+            # prev = prev.next
+
+        # cur = cur.next
 
